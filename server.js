e5b4cedfa66e9e814c3199317c0733afd136a740
davidsdevel <djgm1206@gmail.com>
2022-08-04 04:48:03 -0430

    Fixed routes

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ const app = express();
 const PORT = process.env.PORT || 3007;
 
 const indexGet = require('./lib/index.get');
-const indexPost = require('./lib/index.get');
-const nameGet = require('./lib/index.get');
-const nameDelete = require('./lib/index.get');
+const indexPost = require('./lib/index.post');
+const nameGet = require('./lib/name.get');
+const nameDelete = require('./lib/name.delete');
 
 app.all('/api/image', manageMethods({
   GET: indexGet,
