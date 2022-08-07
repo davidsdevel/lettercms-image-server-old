7c2db1ba43614826ca8cc31f8e9bc023be069941
davidsdevel <djgm1206@gmail.com>
2022-08-07 19:18:45 -0400

    Added CORS

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ global.mongo = require('mongoose');
 
 const express = require('express');
 const manageMethods = require('@lettercms/utils/lib/manageMethods');
+const cors = require('cors');
 
 const app = express();
 
@@ -15,6 +16,15 @@ const indexPost = require('./lib/index.post');
 const nameGet = require('./lib/name.get');
 const nameDelete = require('./lib/name.delete');
 
+const corsOpts = {
+  origin: true,
+  methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
+  credentials: true,
+  exposedHeaders: 'Authorization'
+}
+
+app.use(cors(corsOpts))
 app.all('/api/image', manageMethods({
   GET: indexGet,
   POST: indexPost
